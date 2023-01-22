d521cfbd91ee5e516dd3b40949a00ccc07e43b57
omendavid <davo.dav606@gmail.com>
2023-01-23 00:36:05 +0400

    added some elements in about vue file, still has a lot of changes to do, and has to be made responsive

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,39 @@
 <template lang="">
     <section class="sec-1">
-
+        <div class="s-1-cont">
+            <div class="s-1-f">
+                <div class="s-1-a">
+                    <div class="s-1-a-info">
+                        <h2><span>К</span>то покупает нашу мебель </h2>
+                        <p>Фабрика «Экми-Мебель» — один из ведущих производителей мебели в Украине – открыта к поиску партнёров и предлагает взаимовыгодное сотрудничество. На данный момент дилерская сеть насчитывает более 30 салонов по всей Украине. </p>
+                    </div>
+                    <div class="s-1-a-img-c">
+                        <img src="../assets/img/about/couch.png" alt="">
+                    </div>
+                </div>
+                <div class="s-1-a">
+                    <h2><span>Г</span>де создают нашу мебель</h2>
+                    <div class="s-1-a-middle-cont">
+                        <div class="s-1-a-img-c">
+                            <img src="../assets/img/about/couch.png" alt="">
+                        </div>
+                        <div class="s-1-a-info">
+                            <p>Дилер — это наш партнер и посредник между фабрикой и конечным потребителем. В рамках такого партнерства мы, со стороны фабрики, оказываем любую помощь в работе: предоставляем скидку на выставочные образцы при открытии салона, обеспечиваем всеми материалами с которыми работает фабрика. Предоставляем полностью рабочее программное обеспечение для расчета стоимости мебели и прорисовки модели в 3D.</p>
+                        </div> 
+                    </div>
+                                     
+                </div>
+                <div class="s-1-a">
+                    <div class="s-1-a-info">
+                        <h2><span>Э</span>тапы сотрудничества</h2>
+                        <p>У нас большая складская программа, большинство материалов и фурнитуры постоянно есть на складе.В таком случае, заказы изготавливаются за 13-15 дней.</p>
+                    </div>
+                    <div class="s-1-a-img-c">
+                        <img src="../assets/img/about/couch.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 <script>
@@ -8,6 +41,101 @@ export default {
     
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
+
+    .s-1-cont{
+        max-width: 1064px;
+        margin: 0 auto;
+    }
+
+
+    .s-1-a{
+        color: #343434;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        &:nth-child(1){
+            padding-top: 35px;
+        }
+        &:nth-child(2){
+            padding: 140px 0px 130px 0px;
+            display: flex;
+            flex-direction: column;
+            gap: 40px;
+            .s-1-a-img-c{
+                position: relative;
+                    &::after{
+                        position: absolute;
+                        z-index: -1;
+                        content: '';
+                        width: 282px;
+                        height: 333px;
+                        bottom: -50px;
+                        left: -70px;
+                        background: #5B5B5B;;
+                    }
+            }
+        }
+
+        h2{
+            font-weight: 500;
+            font-size: 30px;
+            letter-spacing:  0.8px;
+            span{
+                color: white;
+                position: relative;
+                &::before{
+                    position: absolute;
+                    z-index: -1;
+                    content: '';
+                    width: 58px;
+                    height: 58px;
+                    bottom: 50%;
+                    right: -2px;
+                    transform: translateY(50%);
+                    background: #A9C2BE;
+                }
+            }
+        }
+        p{
+            font-weight: 300;
+            font-size: 18px;
+            line-height: 150%;
+
+        }
+        
+    }
+
+    .s-1-a-middle-cont{
+        display: flex;
+        width: 94.2%;
+        justify-content: space-between;
+        margin: 0 auto;
+    }
+
+    .s-1-a-info{
+        display: flex;
+        flex-direction: column;
+        gap: 46px;
+        max-width: 530px;
+    }
+
+    .s-1-a-img-c{
+        position: relative;
+            &::after{
+                position: absolute;
+                z-index: -1;
+                content: '';
+                width: 282px;
+                height: 333px;
+                bottom: -50px;
+                right: -70px;
+                background: #A9C2BE;
+            }
+        }
+        
+            
+
     
+
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <About />
   </div>
 </template>
+
+<script>
+import About from '@/components/About.vue';
+
+export default {
+  components: {
+    About,
+}
+}
+</script>
